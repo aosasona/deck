47b34c73f6163813c21264a0007443a633e6bd2b
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-04-17 05:29:42 +0100

    maps

--- lib/user.ex
+++ lib/user.ex
@@ -1,2 +1,16 @@
 defmodule User do
+  def new(first_name, last_name) do
+    email = String.downcase(first_name) <> String.at(String.downcase(last_name), 0)
+
+    %{
+      first_name: first_name,
+      last_name: last_name,
+      email: "#{email}@wyte.space",
+      created_at: Time.utc_now()
+    }
+  end
+
+  def update_email(user, new_email) do
+    %{user | email: new_email}
+  end
 end
